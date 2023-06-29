4441a38be6f3e893bbbed472d0ba175a185a87f1
TessyJames28 <tessyjames28@gmail.com>
2023-06-29 12:03:26 +0100

    Empty class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,8 @@
 // Empty class name ClassRoom
 export default class ClassRoom {
+  /* eslint-disable no-underscore-dangle */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+  /* eslint-enable no-underscore-dangle */
 }
