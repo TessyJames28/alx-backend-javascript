c4752ea092f422a47a8fbad8956c65675d4e209d
TessyJames28 <tessyjames28@gmail.com>
2023-06-29 11:37:30 +0100

    implement new class with getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+// Implementation of a class named HolbertonCourse with 3 attributes
+
+export default class HolbertonCourse {
+  constructor(name, length, students) { 
+    if (typeof(length) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  //getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students
+  }
+
+  //setters
+  set name(value) { 
+    if (typeof(value) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) { 
+    this._length = value;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw TypeError('Students must be an array');
+    }
+
+    for (const val of value) {
+      if (typeof(val) !== 'string') {
+        throw TypeError('Students must be a string array')
+      } 
+    }
+    this._students = value;
+  }
+}
