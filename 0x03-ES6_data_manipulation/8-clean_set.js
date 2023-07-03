62e33fde37a51bdc80f0a0a7f71b96f468afcb2a
TessyJames28 <tessyjames28@gmail.com>
2023-07-04 00:33:32 +0100

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
