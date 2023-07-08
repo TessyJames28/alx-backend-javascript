bda149ab4f0a09297911318519e4ba81c5edcaca
TessyJames28 <tessyjames28@gmail.com>
2023-07-08 06:40:04 +0100

    updates task_1 task

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,41 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+/* An interface for the function named printTeacherFunction */
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const teacherPrinter: printTeacherFunction = printTeacher;
+const result = teacherPrinter("John", "Doe");
+console.log(result); // Output: J. Doe
+
+export interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
