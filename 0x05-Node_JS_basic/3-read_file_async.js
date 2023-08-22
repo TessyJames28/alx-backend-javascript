2af2db5eaac831bbd2a68c86e2574220c72dcafb
TessyJames28 <tessyjames28@gmail.com>
2023-08-22 12:50:08 +0100

    Read file asynchronously with NodeJS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+/* Reading a file synchronously with Node JS */
+const fs = require('fs');
+
+module.exports = function countStudents(database) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(database, 'utf-8', (error, file) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const swe = [];
+      const cs = [];
+      const lines = file.trim().split('\n');
+      const students = lines.length - 1;
+
+      for (const studentLine of lines.slice(1)) {
+        const student = studentLine.split(',').map((item) => item.trim());
+        const field = student[3].trim();
+        if (field === 'CS') {
+          cs.push(student[0]);
+        } else if (field === 'SWE') {
+          swe.push(student[0]);
+        }
+      }
+
+      console.log(`Number of students: ${students}`);
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+      console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+      resolve();
+    });
+  });
+};
