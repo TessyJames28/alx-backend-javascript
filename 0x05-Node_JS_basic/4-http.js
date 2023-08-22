be891a743b43ba8a3ba39489a2343d5da09b0a6c
TessyJames28 <tessyjames28@gmail.com>
2023-08-22 13:52:01 +0100

    Small HTTP server using Node's  HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,13 @@
 /* Create a small HTTP server using Node's HTTP module */
-const app = require('http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-module.exports = server.listen(port, hostname, () => {});
+module.exports = app.listen(port, hostname);
